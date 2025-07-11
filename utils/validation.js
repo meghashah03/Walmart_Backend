188cd0f4b2516b26a2248ebcb2159dbe1680765c
swatitomarr <167579927+swatitomarr@users.noreply.github.com>
2025-07-11 14:46:03 +0530

    add

--- utils/validation.js
+++ utils/validation.js
@@ -1,12 +1,13 @@
-const { validationResult } = require('express-validator');
-
 const validateRequest = (validations) => async (req, res, next) => {
+  try {
   await Promise.all(validations.map((v) => v.run(req)));
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+   const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+    next();
+  } catch (error) {
+    console.error('Validation execution error:', error);
+    return res.status(500).json({ error: 'Internal validation error' });
   }
-  next();
 };
-
-module.exports = { validateRequest };
